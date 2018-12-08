33c632ab412ad895e66e714963b56763a5b07af7
syedbilal2112 <syed.bilal.husssain@mobodexter.com>
2018-12-08 09:49:38 +0530

    first commit

---
+++ file1.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<?php  
+$name = "syed";
+$id = 1;
+$email = "syed@gmal.com";
+
+echo "<table class='table'>";
+echo "<tr><th>Sl:no</th><th>Name</th><th>Email</th></tr>";
+echo "<tr><td>".$id."</td><td>".$name."</td><td>".$email."</td></tr></table>";
+?>
